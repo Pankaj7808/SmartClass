0cf2a73c70433e6ae5272b1d123aef079d8bc2c4
taybaa <tayba3@gmail.com>
2024-07-19 20:58:44 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './pages/login/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage/> //LOOK CAREFULLY
+    </div>
+  );
+}
+
+export default App;
